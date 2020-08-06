dc572d6d6557228bd868873900c8e14de2849426
Edward Withers <dearshrewdwit@gmail.com>
2020-08-06 20:07:02 +0100

    part2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,31 @@
 class DockingStation
+  CAPACITY = 20
   attr_reader :bikes
-  
-  def initialize
+
+  def initialize(capacity: CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
 
   def dock(bike)
+    return "Sorry, full!" if full?
+
     @bikes << bike
   end
 
   def release
+    return "Sorry, none available" if empty?
+
     @bikes.pop
+  end
+
+  private
+
+  def empty?
+    bikes.length == 0
+  end
+
+  def full?
+    bikes.length >= @capacity
   end
 end
